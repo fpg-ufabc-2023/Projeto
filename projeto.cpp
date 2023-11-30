bd6a34a80cdc7102aec3365b9309b2c6b4a71834
Heitor Rodrigues <heitor.rodrigues.savegnago@outlook.com>
2023-11-30 10:54:03 -0300

    correção da proporção na tela

--- projeto.cpp
+++ projeto.cpp
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <unistd.h>     // needed to sleep
+#include <unistd.h> // needed to sleep
 
 #define INV -1
 
@@ -21,26 +21,29 @@ static GLfloat spin = 0.0;
 
 static GLint atom_index = 0;
 
-const GLfloat text_ambient[] = { 0.0, 1.0, 0.0, 1.0 };
+const GLfloat text_ambient[] = {0.0, 1.0, 0.0, 1.0};
 
 /*  Cores Prótons  */
-const GLfloat proton[] = { 1.0, 0.0, 0.0, 1.0 };
-const GLfloat proton_ambient[] = { 0.8, 0.0, 0.0, 1.0 };
+const GLfloat proton[] = {1.0, 0.0, 0.0, 1.0};
+const GLfloat proton_ambient[] = {0.8, 0.0, 0.0, 1.0};
 const GLfloat proton_emission[] = {0.1, 0.0, 0.0, 1.0};
 
 /*  Cores Elétrons  */
-const GLfloat eletron[] = { 0.0, 0.0, 1.0, 1.0 };
+const GLfloat eletron[] = {0.0, 0.0, 1.0, 1.0};
 const GLfloat eletron_ambient[] = {0.0, 0.0, 0.8, 1.0};
 const GLfloat eletron_emission[] = {0.0, 0.0, 0.1, 1.0};
 
 /*  Cores Neutrons  */
-const GLfloat neutron[] = { 0.5, 0.5, 0.5, 1.0 };
+const GLfloat neutron[] = {0.5, 0.5, 0.5, 1.0};
 const GLfloat neutron_ambient[] = {0.4, 0.4, 0.4, 1.0};
 const GLfloat neutron_emission[] = {0.05, 0.05, 0.05, 1.0};
 
-const GLfloat high_shininess[] = { 100.0 };
+const GLfloat high_shininess[] = {100.0};
 
-typedef struct Atom{
+char vec[100];
+
+typedef struct Atom
+{
 	char sigla[3];
 	int protons;
 	int neutrons;
@@ -49,49 +52,48 @@ typedef struct Atom{
 } Atom;
 
 const Atom decay[] = {
-	{"U",  92, 238-92,   1, INV}, // 0
-	{"Th", 90, 234-90, INV,   2}, // 1
-	{"Pa", 91, 234-91, INV,   3}, // 2
-	{"U",  92, 234-92,   4, INV}, // 3
-	{"Th", 90, 230-90,   5, INV}, // 4
-	{"Ra", 88, 226-88,   6, INV}, // 5
-	{"Rn", 86, 222-86,   7, INV}, // 6
-	{"Po", 84, 218-84,   8,   9}, // 7
-	{"Pb", 82, 214-82, INV,  10}, // 8
-	{"At", 84, 218-85,  10, INV}, // 9
-	{"Bi", 83, 214-83,  11,  12}, // 10
-	{"Ti", 81, 210-81, INV,  13}, // 11
-	{"Po", 84, 214-84,  13, INV}, // 12
-	{"Pb", 82, 210-82,  14,  15}, // 13
-	{"Hg", 80, 206-80, INV,  16}, // 14
-	{"Bi", 83, 210-83,  16,  17}, // 15
-	{"Ti", 81, 206-81, INV,  18}, // 16
-	{"Po", 84, 210-84,  18, INV}, // 17
-	{"Pb", 82, 206-82, INV, INV}, // 18
+	{"U", 92, 238 - 92, 1, INV},	// 0
+	{"Th", 90, 234 - 90, INV, 2},	// 1
+	{"Pa", 91, 234 - 91, INV, 3},	// 2
+	{"U", 92, 234 - 92, 4, INV},	// 3
+	{"Th", 90, 230 - 90, 5, INV},	// 4
+	{"Ra", 88, 226 - 88, 6, INV},	// 5
+	{"Rn", 86, 222 - 86, 7, INV},	// 6
+	{"Po", 84, 218 - 84, 8, 9},		// 7
+	{"Pb", 82, 214 - 82, INV, 10},	// 8
+	{"At", 84, 218 - 85, 10, INV},	// 9
+	{"Bi", 83, 214 - 83, 11, 12},	// 10
+	{"Ti", 81, 210 - 81, INV, 13},	// 11
+	{"Po", 84, 214 - 84, 13, INV},	// 12
+	{"Pb", 82, 210 - 82, 14, 15},	// 13
+	{"Hg", 80, 206 - 80, INV, 16},	// 14
+	{"Bi", 83, 210 - 83, 16, 17},	// 15
+	{"Ti", 81, 206 - 81, INV, 18},	// 16
+	{"Po", 84, 210 - 84, 18, INV},	// 17
+	{"Pb", 82, 206 - 82, INV, INV}, // 18
 };
 
-const char* text_Atom[]={
-		"Massa: 238 \nN° atômico: 92",
-		"Massa: 234 \nN° atômico: 90",
-		"Massa: 234 \nN° atômico: 91",
-		"Massa: 234 \nN° atômico: 92",
-		"Massa: 230 \nN° atômico: 90",
-		"Massa: 226 \nN° atômico: 88",
-		"Massa: 222 \nN° atômico: 86",
-		"Massa: 218 \nN° atômico: 84",
-		"Massa: 214 \nN° atômico: 82",
-		"Massa: 218 \nN° atômico: 85",
-		"Massa: 214 \nN° atômico: 83",
-		"Massa: 210 \nN° atômico: 81",
-		"Massa: 214 \nN° atômico: 84",
-		"Massa: 210 \nN° atômico: 82",
-		"Massa: 206 \nN° atômico: 80",
-		"Massa: 210 \nN° atômico: 83",
-		"Massa: 206 \nN° atômico: 81",
-		"Massa: 210 \nN° atômico: 84",
-		"Massa: 206 \nN° atômico: 82",	
+const char *text_Atom[] = {
+	"Massa: 238 \nN° atômico: 92",
+	"Massa: 234 \nN° atômico: 90",
+	"Massa: 234 \nN° atômico: 91",
+	"Massa: 234 \nN° atômico: 92",
+	"Massa: 230 \nN° atômico: 90",
+	"Massa: 226 \nN° atômico: 88",
+	"Massa: 222 \nN° atômico: 86",
+	"Massa: 218 \nN° atômico: 84",
+	"Massa: 214 \nN° atômico: 82",
+	"Massa: 218 \nN° atômico: 85",
+	"Massa: 214 \nN° atômico: 83",
+	"Massa: 210 \nN° atômico: 81",
+	"Massa: 214 \nN° atômico: 84",
+	"Massa: 210 \nN° atômico: 82",
+	"Massa: 206 \nN° atômico: 80",
+	"Massa: 210 \nN° atômico: 83",
+	"Massa: 206 \nN° atômico: 81",
+	"Massa: 210 \nN° atômico: 84",
+	"Massa: 206 \nN° atômico: 82",
 };
-
 
 /**********************************************************************/
 /*                        Declaração de funções                       */
@@ -110,15 +112,13 @@ void draw_particle(void);
 void draw_eletron(void);
 void draw_proton(void);
 void draw_neutron(void);
-void render_text(const char* text, int x, int y);
-
+void render_text(const char *text, int x, int y);
 
 /**********************************************************************/
 /*                                                                    */
 /*                       Função principal (main)                      */
 /*                                                                    */
 /**********************************************************************/
-
 
 int main(int argc, char **argv)
 {
@@ -131,7 +131,6 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
 
 /**********************************************************************/
 /*                                                                    */
@@ -149,7 +148,7 @@ void init_glut(const char *nome_janela, int *argcp, char **argv)
 	glutInit(argcp, argv);
 
 	/* inicia o display usando RGB e double-buffering */
-   	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(1000, 1000);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow(nome_janela);
@@ -162,23 +161,23 @@ void init_glut(const char *nome_janela, int *argcp, char **argv)
 	glutMouseFunc(mouse_callback);
 	glutIdleFunc(animate_callback);
 
-    /* define o menu */
-    glutCreateMenu(menu_callback);
-    glutAddMenuEntry("Decaimento Alfa", 0);
-    glutAddMenuEntry("Decaimento Beta", 1);
-    glutAddMenuEntry("Sair", 2);
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
+	/* define o menu */
+	glutCreateMenu(menu_callback);
+	glutAddMenuEntry("Decaimento Alfa", 0);
+	glutAddMenuEntry("Decaimento Beta", 1);
+	glutAddMenuEntry("Sair", 2);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-	GLfloat ambient[] = { 0.0, 0.0, 0.0, 1.0 };
-	GLfloat diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat position[] = { 0.0, 1.0, -1.0, 0.0 };
-	GLfloat lmodel_ambient[] = { 0.4, 0.4, 0.4, 1.0 };
-	GLfloat local_view[] = { 0.0 };
+	GLfloat ambient[] = {0.0, 0.0, 0.0, 1.0};
+	GLfloat diffuse[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat specular[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat position[] = {0.0, 1.0, 1.0, 0.0};
+	GLfloat lmodel_ambient[] = {0.4, 0.4, 0.4, 1.0};
+	GLfloat local_view[] = {0.0};
 
 	/* define a cor com a qual a tela será apagado */
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glEnable(GL_DEPTH_TEST);		        // Enables Depth Testing
+	glEnable(GL_DEPTH_TEST); // Enables Depth Testing
 	glShadeModel(GL_SMOOTH);
 
 	// glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
@@ -191,7 +190,7 @@ void init_glut(const char *nome_janela, int *argcp, char **argv)
 	// glDepthFunc(GL_LESS);			        // The Type Of Depth Test To Do
 
 	glEnable(GL_LIGHTING);
-   	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT0);
 
 	// glMatrixMode(GL_PROJECTION);
 
@@ -208,83 +207,90 @@ void draw_object(void)
 	const GLint neutron_number = decay[atom_index].neutrons;
 
 	glPushMatrix();
-		for(int i = 0; i < proton_number; i++){
-			glPushMatrix();
-				glRotatef ((GLfloat) i * 360.0 / proton_number , 0.0, 1.0, 0.0);
-				glTranslatef (0.6-0.3*(i%5), 0.0, 0.0);
-				draw_proton();
-			glPopMatrix();
-		}
+	for (int i = 0; i < proton_number; i++)
+	{
+		glPushMatrix();
+		glRotatef((GLfloat)i * 360.0 / proton_number, 0.0, 1.0, 0.0);
+		glTranslatef(0.6 - 0.3 * (i % 5), 0.0, 0.0);
+		draw_proton();
+		glPopMatrix();
+	}
 
-		for(int i = 0; i < neutron_number; i++){
-			glPushMatrix();
-				glRotatef ((GLfloat) (i * 360.0 / neutron_number + 180.0/proton_number) , 0.0, 1.0, 0.0);
-				glTranslatef (0.4+0.2*(i%3), 0.0, 0.0);
-				draw_neutron();
-			glPopMatrix();
-		}
+	for (int i = 0; i < neutron_number; i++)
+	{
+		glPushMatrix();
+		glRotatef((GLfloat)(i * 360.0 / neutron_number + 180.0 / proton_number), 0.0, 1.0, 0.0);
+		glTranslatef(0.4 + 0.2 * (i % 3), 0.0, 0.0);
+		draw_neutron();
+		glPopMatrix();
+	}
 
-		for(int i = 0; i < proton_number; i++){
-			glPushMatrix();
-				glRotatef ((GLfloat) spin + i * 360.0 / proton_number, 0.0, 1.0, 0.0);
-				glTranslatef (2.0+0.5*(i%5), 0.0, 0.0);
-				draw_eletron();
-			glPopMatrix();
-		}
+	for (int i = 0; i < proton_number; i++)
+	{
+		glPushMatrix();
+		glRotatef((GLfloat)spin + i * 360.0 / proton_number, 0.0, 1.0, 0.0);
+		glTranslatef(2.0 + 0.5 * (i % 5), 0.0, 0.0);
+		draw_eletron();
+		glPopMatrix();
+	}
 	glPopMatrix();
 }
 
-void render_text(const char* text, int x, int y){
-		glRasterPos3i(x,y,1);
-		
-		for(int i=0; text[i]!= '\0'; ++i){
-			
-			if (text[i] =='\n'){ //caso de quebra de linha
-				y = y-20;
-				glRasterPos3i(x,y,1);
-			}
-			
-			else{
-				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
-			}
+void render_text(const char *text, int x, int y)
+{
+	glRasterPos3i(x, y, 1);
+
+	for (int i = 0; text[i] != '\0'; ++i)
+	{
+
+		if (text[i] == '\n')
+		{ // caso de quebra de linha
+			y = y - 20;
+			glRasterPos3i(x, y, 1);
 		}
+
+		else
+		{
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+		}
+	}
 }
 
-void interface_text(){
-	const char* menu = decay[atom_index].sigla;
-	
-	glMatrixMode( GL_PROJECTION );
+void interface_text()
+{
+	const char *menu = decay[atom_index].sigla;
+
+	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
 	glLoadIdentity();
-	gluOrtho2D( 0, 500, 0, 500 );
+	gluOrtho2D(0, 500, 0, 500);
 
-	glMatrixMode( GL_MODELVIEW );
+	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
 	glLoadIdentity();
 	// glRasterPos2i( 200, 100 );
-	glRasterPos3i( 200, 100, 1 );
+	glRasterPos3i(200, 100, 1);
 
 	// glColor3f( 1.0, 0.0, 0.0 );
-	for ( int i = 0; menu[i] != '\0'; ++i ) {
+	for (int i = 0; menu[i] != '\0'; ++i)
+	{
 		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, menu[i]);
 	}
-	
-	
-	
-	const char* text = text_Atom[atom_index];
-	
-	render_text(text, 200,100 -30);
-	
+
+	const char *text = text_Atom[atom_index];
+
+	render_text(text, 200, 100 - 30);
+
 	glPopMatrix();
 
-	glMatrixMode( GL_PROJECTION );
+	glMatrixMode(GL_PROJECTION);
 	glPopMatrix();
-	glMatrixMode( GL_MODELVIEW );
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void display_callback(void)
 {
-	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	draw_object();
 	interface_text();
@@ -292,11 +298,13 @@ void display_callback(void)
 	glutSwapBuffers();
 }
 
-void draw_particle(void){
+void draw_particle(void)
+{
 	glutSolidSphere(0.2, 10, 8);
 }
 
-void draw_eletron(void){
+void draw_eletron(void)
+{
 	glMaterialfv(GL_FRONT, GL_AMBIENT, eletron_ambient);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, eletron);
 	glMaterialfv(GL_FRONT, GL_EMISSION, eletron_emission);
@@ -307,7 +315,8 @@ void draw_eletron(void){
 	glMaterialfv(GL_FRONT, GL_EMISSION, text_ambient);
 }
 
-void draw_proton(void){
+void draw_proton(void)
+{
 	glMaterialfv(GL_FRONT, GL_AMBIENT, proton_ambient);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, proton);
 	glMaterialfv(GL_FRONT, GL_EMISSION, proton_emission);
@@ -318,7 +327,8 @@ void draw_proton(void){
 	glMaterialfv(GL_FRONT, GL_EMISSION, text_ambient);
 }
 
-void draw_neutron(void){
+void draw_neutron(void)
+{
 	glMaterialfv(GL_FRONT, GL_AMBIENT, neutron_ambient);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, neutron);
 	glMaterialfv(GL_FRONT, GL_EMISSION, neutron_emission);
@@ -343,24 +353,26 @@ void reshape_callback(int w, int h)
 	glLoadIdentity();
 
 	/* adapta a projec��o ortogonal de acordo com as dimens�es da janela */
-	if (h > w)
-	{
-		gluOrtho2D(-1.0, 1.0, 1.0*((GLfloat)-h / (GLfloat)w), 1.0*((GLfloat)h / (GLfloat)w));
-	}
+	if (w <= h)
+		glOrtho(-6.0, 6.0,
+				-6.0 * ((GLfloat)h) / (GLfloat)w,
+				6.0 * ((GLfloat)h) / (GLfloat)w,
+				-10.0, 10.0);
 	else
-	{
-		gluOrtho2D(1.0*((GLfloat)-w / (GLfloat)h), 1.0*((GLfloat)w / (GLfloat)h), -1.0, 1.0);
-	}
+		glOrtho(-6.0 * (GLfloat)w / ((GLfloat)h),
+				6.0 * (GLfloat)w / ((GLfloat)h),
+				-6.0, 6.0, -10.0, 10.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-   	gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+	gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
 
 	/* muda para o modo GL_MODELVIEW (n�o pretendemos alterar a projec��o *
 	 * quando estivermos a desenhar na tela)                              */
 	glMatrixMode(GL_MODELVIEW);
 
-
 	glLoadIdentity();
-	gluLookAt (0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0);
+	gluLookAt(0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0);
 
 	return;
 }
@@ -379,15 +391,19 @@ void animate_callback(void)
  */
 void keyboard_callback(unsigned char key, int x, int y)
 {
+	sprintf(vec, "Massa: %d \nN° atômico: %d", decay[atom_index].protons + decay[atom_index].neutrons, decay[atom_index].protons);
+	printf("%s\n", vec);
 	switch (key)
 	{
 	case 'a':
 	case 'A':
-		if(decay[atom_index].alfa > INV) atom_index = decay[atom_index].alfa;
+		if (decay[atom_index].alfa > INV)
+			atom_index = decay[atom_index].alfa;
 		break;
 	case 'b':
 	case 'B':
-		if(decay[atom_index].beta > INV) atom_index = decay[atom_index].beta;
+		if (decay[atom_index].beta > INV)
+			atom_index = decay[atom_index].beta;
 		break;
 	case 'u':
 	case 'U':
@@ -432,36 +448,41 @@ void keyboard_callback_special(int key, int x, int y)
 /*
  * Função callback para o controlo do menu
  */
-void menu_callback(int value){
-    switch(value){
-        case 0:
-			if(decay[atom_index].alfa > INV) atom_index = decay[atom_index].alfa;
+void menu_callback(int value)
+{
+	switch (value)
+	{
+	case 0:
+		if (decay[atom_index].alfa > INV)
+			atom_index = decay[atom_index].alfa;
 		break;
 
-        case 1:
-			if(decay[atom_index].beta > INV) atom_index = decay[atom_index].beta;
-            break;
+	case 1:
+		if (decay[atom_index].beta > INV)
+			atom_index = decay[atom_index].beta;
+		break;
 
-        case 2:
-            exit(0);                    /* Opção 4: sai do programa */
-            break;
+	case 2:
+		exit(0); /* Opção 4: sai do programa */
+		break;
 
-        default:
-            break;
-    }
+	default:
+		break;
+	}
 
-    /* Manda o redesenhar o ecr� quando o menu for desactivado */
-    glutPostRedisplay();
+	/* Manda o redesenhar o ecr� quando o menu for desactivado */
+	glutPostRedisplay();
 
-    return;
+	return;
 }
 
 void spinDisplay(void)
 {
 	spin = spin + 0.5;
-	if (spin > 360.0) spin = spin - 360.0;
+	if (spin > 360.0)
+		spin = spin - 360.0;
 	glutPostRedisplay();
-    usleep(100);
+	usleep(100);
 }
 
 void mouse_callback(int button, int state, int x, int y)
@@ -483,4 +504,3 @@ void mouse_callback(int button, int state, int x, int y)
 		break;
 	}
 }
-
